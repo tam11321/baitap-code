1d9bb76bfd7317ef8cb35007eba961cb48f1177f
allain <112264031+tam11321@users.noreply.github.com>
2022-12-19 12:41:37 +0700

    Add files via upload

---
+++ namnhuan(2).cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	int thang,nam;
+	cout<<" nhap thang: ";cin>>thang;
+	cout<<" nhap nam:";cin>>nam;
+	
+	switch(thang)
+	{
+		case 1:
+		case 3:
+		case 5:
+		case 7:
+		case 8:
+		case 10:
+		case 12:
+			cout<<"co 31 ngay"; break;
+		case 4:
+		case 6:
+		case 9:
+		case 11:
+			cout<<"co 30 ngay"; break;
+		case 2:
+			if ( nam %400==0 || (nam %4 && nam % 100 != 0))
+			{ 
+			cout<<" co 29 ngay ";	
+			}	
+			else
+			{
+				cout<<" co 28 ngay";
+			}
+			break;
+		default:
+			cout<<" nhap du lieu khong dung";
+		
+	}
+}
