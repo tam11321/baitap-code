cbd6aff418b12b7e506930bb5bc467ff7302e78f
allain <112264031+tam11321@users.noreply.github.com>
2023-03-06 11:58:59 +0700

    Add files via upload

---
+++ chuoi.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<math.h>
+#include<string.h>
+using namespace std;
+
+int main()
+{
+	
+//	int n;
+//	cin>>n;//goi ten ham khong co khoang trang
+//	cin.ignore();//loai bo phim enter
+//	string s; //goi ten ham
+//	getline(cin,s);//goi ten ham co khoang trang
+//	cout<<s<<endl;
+//	return 0;
+	int n;
+	char ten[50];
+	cout<<"hay nhap ten cua ban:";
+	gets(ten);
+	cout<<"xin chao ban ";
+	puts(ten);
+	cout<<"do dai ten cua ban la "<<strlen(ten);
+	
+}
